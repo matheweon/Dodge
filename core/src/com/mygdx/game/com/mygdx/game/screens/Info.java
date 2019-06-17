7da90eb513285569cab2507f4b612ff900e575d7
Mathew Seng <gigamatt123@gmail.com>
2019-06-16 21:39:38 -0400

    .

--- core/src/com/mygdx/game/com/mygdx/game/screens/Info.java
+++ core/src/com/mygdx/game/com/mygdx/game/screens/Info.java
@@ -31,8 +31,8 @@ public class Info implements Screen {
     private static final int BUTTON_WIDTH = 300;
     private static final int BUTTON_HEIGHT = 100;
     private static final int HOME_BUTTON_SIZE = 100;
-    private static final int HOME_BUTTON_XVAL = 0;
-    private static final int HOME_BUTTON_YVAL = DodgeGame.HEIGHT - HOME_BUTTON_SIZE;
+    private static final int HOME_BUTTON_X_VALUE = 0;
+    private static final int HOME_BUTTON_Y_VALUE = DodgeGame.HEIGHT - HOME_BUTTON_SIZE;
 
     private static final int MUTE_BUTTON_Y_VALUE = (int) (DodgeGame.HEIGHT * 0.01);
     private static final int CLEAR_BUTTON_Y_VALUE = (int) (DodgeGame.HEIGHT * 0.16);
@@ -135,11 +135,11 @@ public class Info implements Screen {
         }
 
         //displays home button and see if it's clicked
-        game.batch.draw(homeButton, HOME_BUTTON_XVAL, HOME_BUTTON_YVAL, HOME_BUTTON_SIZE, HOME_BUTTON_SIZE);
-        if (Gdx.input.getX() > HOME_BUTTON_XVAL &&
-                Gdx.input.getX() < HOME_BUTTON_XVAL + HOME_BUTTON_SIZE &&
-                DodgeGame.HEIGHT - Gdx.input.getY() > HOME_BUTTON_YVAL &&
-                DodgeGame.HEIGHT - Gdx.input.getY() < HOME_BUTTON_YVAL + HOME_BUTTON_SIZE &&
+        game.batch.draw(homeButton, HOME_BUTTON_X_VALUE, HOME_BUTTON_Y_VALUE, HOME_BUTTON_SIZE, HOME_BUTTON_SIZE);
+        if (Gdx.input.getX() > HOME_BUTTON_X_VALUE &&
+                Gdx.input.getX() < HOME_BUTTON_X_VALUE + HOME_BUTTON_SIZE &&
+                DodgeGame.HEIGHT - Gdx.input.getY() > HOME_BUTTON_Y_VALUE &&
+                DodgeGame.HEIGHT - Gdx.input.getY() < HOME_BUTTON_Y_VALUE + HOME_BUTTON_SIZE &&
                 Gdx.input.justTouched() ||
                 Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             music.stop();
